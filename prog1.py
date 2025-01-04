4578b8d0eb00c66fc063c8a8ec2e47dc32115f73
MYousaf92 <mohammadyusf92@gmail.com>
2025-01-04 08:00:27 +0500

    python p1, p2
    just for practice code

---
+++ prog1.py
@@ -0,0 +1,36 @@
+
+
+
+print ("cout")
+
+
+x = 3.12
+print (x)
+
+str = "Yusuf"
+print (str)
+
+# username = input ("Enter user name: ") 
+# print ("The user_name is " + username)
+
+
+
+
+x = int (input ("Enter any number: "))
+y = int (input ("Enter second nuber: "))
+
+print (x)
+
+# print ("The value of x is = " + str(x))
+print (f"The value of x is = {x}")
+print (f"The value of y is = {y}")
+print (x + y)
+
+q= float(input("Enter your decimal value= "))
+r= float(input("Enter your decimal value= "))
+z= q*r
+print (f"The value of z = " + round(z,2))
+print (round(z, 2))
+# a = 10 
+# b = 5
+# print (a + b)
---
+++ prog2.py
@@ -0,0 +1,78 @@
+
+'''
+if expression:
+    print ('')
+    print ('')
+elif expression: 
+    print ('')
+else:
+    print ('')
+'''
+
+
+'''
+val = float (input ("Enter float value: "))
+print ('Float value = ' + str(val))
+print ('Float value = ', val)
+print (f'Float value = {val}')
+print ("Enter ahmad's address: ")
+'''
+
+ 
+'''
+a = 10
+b = 16
+c = 15
+
+if a > b and a > c: 
+    print (a)
+elif b > a and b > c:
+    print ('B')
+    print (b)
+else: 
+    print (c)
+'''
+
+'''choice = int (input ("Enter number: "))
+match choice: 
+    case 1: 
+        print ('Option 1')
+    case 2: 
+        print ('Option 2')
+    case 3: 
+        print ('Option 3')
+    case _: 
+        print ("Unknown")'''
+        
+
+# for i in range(0, 10): # for (int i=0; i<10; i++)
+#     print ('Hello World')
+    
+    
+# for k in range(0, 10, 3): # for (int k=0; k<10; k+=3)
+#     print ('HAHAHA!!!')
+
+# items = ['A', True, False, 1.1, 'B', 'C', 'D', 'E', 'F', 'G']
+# for i in items:
+#     if (i == 'B' or i == 'C' or i == 'D'):
+#         continue
+#     print (i)
+    
+    
+    
+# arr = []
+# x = -1
+# while x != 0:
+#     x = int (input ('Enter any integer (or 0 to exit the loop): ')) 
+#     if (x == 0):
+#         continue
+#     arr.append(x)
+
+
+# for index in arr: 
+#     print (index, end=" ")
+
+
+x = int (input ('Enter your table no='))
+for i in range(1,11):
+ print(f'{x} x {i} = {x*i}')   
